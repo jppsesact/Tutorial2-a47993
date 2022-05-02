2069bd54fce13cc0f617c554ed406d12d15567ec
MarcoSantosa47993 <a47993@alunos.ipb.pt>
2022-05-02 16:04:40 +0100

    7.Check the Enemy's Line of Sight is Clear

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Observer : MonoBehaviour
 {
     public Transform player;
+    public GameEnding gameEnding;
 
     bool m_IsPlayerInRange;
 
@@ -21,6 +22,24 @@ public class Observer : MonoBehaviour
         if (other.transform == player)
         {
             m_IsPlayerInRange = false;
+        }
+    }
+
+    void Update ()
+    {
+        if (m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    
+                }
+            }
         }
     }
 }
